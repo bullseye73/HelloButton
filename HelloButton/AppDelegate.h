5db8def6b5339eb8f920892ca0785a1bc29d5099
Claude C. Jeong <bullseye.dev2@gmail.com>
2017-12-27 17:38:44 +0900

    Initial Commit

---
+++ HelloButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloButton
+//
+//  Created by Claude C. Jeong on 2017. 12. 27..
+//  Copyright © 2017년 Claude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloButton
+//
+//  Created by Claude C. Jeong on 2017. 12. 27..
+//  Copyright © 2017년 Claude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
