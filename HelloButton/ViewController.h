636ceb0b3ca04e033fa28767976aa875926f6bec
Claude C. Jeong <bullseye.dev2@gmail.com>
2017-12-29 11:06:38 +0900

    UI 수정 iphoneX로 바꿈

--- HelloButton/ViewController.h
+++ HelloButton/ViewController.h
@@ -7,9 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
 
 @interface ViewController : UIViewController
 
+
+@property (weak, nonatomic) IBOutlet UISwitch *switchBtn;
+
+- (IBAction)onBtnTouch:(UIButton *)sender ;
+- (IBAction)OnSwitchValueChange:(id)sender;
+@property (weak, nonatomic) IBOutlet UIView *stWebView;
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *titleSwitch;
+@property (weak, nonatomic) IBOutlet WKWebView *webView;
 
 @end
 
--- HelloButton/ViewController.m
+++ HelloButton/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <WKNavigationDelegate>
 
 @end
 
@@ -16,14 +16,57 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"Start---");
+    _titleLabel.text=@"";
+    _titleSwitch.text=@"";
+    
+    //self.webView = [[WKWebView alloc] initWithFrame:self.view.frame];
+    
+    self.webView = [[WKWebView alloc] initWithFrame:self.stWebView.frame];
+    [self.webView setNavigationDelegate:self];
+    [self.view addSubview:self.webView];
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"https://www.google.co.kr"]];
+    [self.webView loadRequest:request];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)onBtnTouch:(UIButton *)sender {
+    NSLog(@"test button touch");
+    
+    //NSURL *myURL = [NSURL URLWithString:@"http://www.google.com"];
+    //NSURLRequest *myURLReq = [NSURLRequest requestWithURL:myURL];
+    //[self.webView loadRequest:myURLReq];
+    
+    _titleLabel.text = @"Hello Button test";
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)OnSwitchValueChange:(id)sender {
+    NSLog(@"Switch touch");
+    
+    _titleSwitch.text = [NSString stringWithFormat:@"The switch is %@", _switchBtn.isOn ? @"ON" : @"OFF"];
+}
+
+#pragma mark - WKNavigationDelegate
+- (void)webView:(WKWebView *)webView didCommitNavigation:(null_unspecified WKNavigation *)navigation {
+    NSLog(@"1. didCommitNavigation");
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    NSLog(@"2. didFinishNavigation");
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
+    NSLog(@"3. didFailNavigation");
+}
 
 @end
